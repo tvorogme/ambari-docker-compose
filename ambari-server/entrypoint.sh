75e241c8b7a53e3ba63fd6401c1952d7de391b6e
Andrey Tvorozhkov <tvorog@tvorog.me>
2020-10-15 19:41:01 +0300

    Fix

--- ambari-server/entrypoint.sh
+++ ambari-server/entrypoint.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ambari-server setup -j /usr/jdk64/jdk1.8.0_112 --database=postgres --databasehost=postgres --databaseusername=headlabs --databasepassword=headlabs --databaseport=5432 --databasename=headlabs -s
 PGPASSWORD=headlabs psql --host postgres --user headlabs -f /var/lib/ambari-server/resources/Ambari-DDL-Postgres-CREATE.sql
 ambari-server start
